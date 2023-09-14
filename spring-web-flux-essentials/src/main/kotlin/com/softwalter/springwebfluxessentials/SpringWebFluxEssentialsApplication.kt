b03d806eaad20a5a750a3457e161ced0496f3755
WalterHeitor <waterheitorfo@gmail.com>
2023-09-13 22:49:44 -0300

    feat: intit project

---
+++ spring-web-flux-essentials/src/main/kotlin/com/softwalter/springwebfluxessentials/SpringWebFluxEssentialsApplication.kt
@@ -0,0 +1,11 @@
+package com.softwalter.springwebfluxessentials
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebFluxEssentialsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringWebFluxEssentialsApplication>(*args)
+}
